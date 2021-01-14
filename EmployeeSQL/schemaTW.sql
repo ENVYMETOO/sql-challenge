2d979dd871714ce107d8dfb0da3e1f92ba95d62c
tamara washington <tamey.washington@gmail.com>
2021-01-14 11:26:27 -0600

    Adding schemas

---
+++ EmployeeSQL/schemaTW.sql
@@ -0,0 +1,24 @@
+DROP TABLE departments;
+
+CREATE TABLE departments(
+	dept_no varchar NOT null,
+	dept_name varchar Not null,
+	PRIMARY KEY (dept_no)
+);
+
+select * from departments;
+
+CREATE TABLE dept_employee(
+);
+
+CREATE TABLE titles(
+);
+
+CREATE TABLE dept_manager(
+);
+
+CREATE TABLE employees(
+);
+
+CREATE TABLE salaries(
+);
